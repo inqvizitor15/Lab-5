1ccfec67d2b2c1d86a58247a1c85ed323e910151
Gwynbleidd <ushanov1000@gmail.com>
2024-12-30 18:58:55 +0300

    up1

--- lab5.py
+++ lab5.py
@@ -1,13 +1,16 @@
 import re
 
+# --------Задание 1------
 with open('task1-en.txt', 'r') as file:
     data = file.read()
-
-    #--------Задание 1------
     rez1 = re.findall(r'\b\w+\b,', data)
     rez1_1 = re.findall(r"\[([^]]*)\]", data)
 
-    # --------Задание 2------
-    rez2 = re.findall(r"#(?:[0-9a-fA-F]{3}){1,2}", data)
+# --------Задание 2------
+with open('task2.html', 'r', encoding='utf-8') as file2:
+    rez2 = []
+    for line in file2:
+        if len((temp := re.findall(r"#(?:[0-9a-fA-F]{3}){1,2}", line))) > 0:
+            rez2.append(temp)
 
 print(rez2)
