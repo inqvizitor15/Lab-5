d2ea9956020aa2f31a7abcb68c485d7c31276b48
Gwynbleidd <ushanov1000@gmail.com>
2024-12-30 22:03:37 +0300

    up1

--- lab5.py
+++ lab5.py
@@ -21,14 +21,14 @@ print(rez2)
 with open("task3.txt", "r", encoding="utf-8") as file:
     data = file.read()
 
-id = re.findall(r"\b\d+\b", data)
-name = re.findall(r"\b[A-Z][a-z]+(?:\s[A-Z][a-z]+)*\b", data)
-email = re.findall(r"[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,}", data)
-date = re.findall(r"\b\d{4}-\d{2}-\d{2}\b", data)
-website = re.findall(r"https?://(?:www\.)?\S+", data)
+    id = re.findall(r"\b\d+\b", data)
+    name = re.findall(r"\b[A-Z][a-z]+(?:\s[A-Z][a-z]+)*\b", data)
+    email = re.findall(r"[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,}", data)
+    date = re.findall(r"\b\d{4}-\d{2}-\d{2}\b", data)
+    website = re.findall(r"https?://(?:www\.)?\S+", data)
 
 records = zip(id, name, email, date, website)
-with open("result.csv", "w", encoding="utf-8", newline="") as csvfile: # заполнение таблицы
+with open("rez3.csv", "w", encoding="utf-8", newline="") as csvfile: # заполнение таблицы
     writer = csv.writer(csvfile)
     writer.writerow(["ID", "Name", "Email", "Date", "Website"])
     writer.writerows(records)
